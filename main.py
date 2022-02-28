7a13d0643fa9e78dc49ca689b66144db9f666795
Pulsar94 <94862548+Pulsar94@users.noreply.github.com>
2022-02-28 15:56:02 +0100

    init

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-"""
-test
-"""
+import pygame
+
+#initialisation de pygame
+pygame.init()
+
+#creation écran
+screen = pygame.display.set_mode((800,600))
+
+continuer = True
+
+while continuer:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            continuer = False
+
+pygame.quit()
