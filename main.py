3842a3ec5ef49d548e96dc4c417c53e1ccce1f03
Pulsar94 <94862548+Pulsar94@users.noreply.github.com>
2022-03-14 18:45:09 +0100

    background

--- main.py
+++ main.py
@@ -5,13 +5,26 @@ pygame.init()
 
 #creation écran
 pygame.display.set_caption("Crabo-Braquo") #possibilité de rajouté une icone dans la fenètre en mettant une virgule
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((1000,394))
+running = True
 
-continuer = True
 
-while continuer:
+# importer un fond d'écran
+background = pygame.image.load("assets/desert.png")
+
+
+while running:
+
+    # appliquer l'image sur l'arrière plan du jeu
+    screen.blit(background, (0,0))
+
+    # mettre a jour l'écran
+    pygame.display.flip()
+
+    # condition pour fermer la fenêtre
     for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            continuer = False
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
 
 pygame.quit()
