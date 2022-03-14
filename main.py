22c74160381c1eb671b139fd54486cd621eb32ab
Pulsar94 <94862548+Pulsar94@users.noreply.github.com>
2022-03-14 17:28:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pygame
 pygame.init()
 
 #creation écran
+pygame.display.set_caption("Crabo-Braquo") #possibilité de rajouté une icone dans la fenètre en mettant une virgule
 screen = pygame.display.set_mode((800,600))
 
 continuer = True
