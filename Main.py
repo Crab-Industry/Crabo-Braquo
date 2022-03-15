54d0447a63c782e775f19c375d5136d9110cdfbd
SpringFsm <fabio.mata@efrei.net>
2022-03-15 11:24:12 +0100

    Background, canon, logo et titre

--- Main.py
+++ Main.py
@@ -6,16 +6,16 @@ pygame.init()
 screen = pygame.display.set_mode((800,600))
 
 #background
-background = pygame.image.load('background.jpg')
+background = pygame.image.load('images/background.jpg')
 
 #titre et icône
 pygame.display.set_caption("Crabo Braquo")
-icon = pygame.image.load('crabe.png')
+icon = pygame.image.load('images/crabe.png')
 pygame.display.set_icon(icon)
 
 #joueur
 
-playerImg = pygame.image.load('canon.png')
+playerImg = pygame.image.load('images/canon.png')
 playerX = 10
 playerY = 500
 
