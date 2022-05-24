239ffdba8f578cb9cf386b3e880f9c9e30995ce0
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2022-05-24 23:16:20 +0200

    Commentary on sound.py

--- sound.py
+++ sound.py
@@ -7,7 +7,17 @@ import pygame
 
 
 class Soundplayer:
+    """
+    Classe Soundplayer
+    Permet la gestion des sons dans le programme
+    """
     def __init__(self):
+        """
+        Fonction: __init__
+        --------------------
+        self.son: dictionnaire qui stock le chemin des musiques/sons
+        Format: .ogg
+        """
         self.son = {
             "click": pygame.mixer.Sound("assets/sound/click.ogg"),
             "canon": pygame.mixer.Sound("assets/sound/canon.ogg"),
@@ -16,6 +26,14 @@ class Soundplayer:
         }
 
     def play_sound(self, track):
+        """
+        Fonction: play_sound
+        --------------------
+        Permet de jouer de la musique/sons
+
+        :param:
+            track: (str) musique présent dans Soundplayer
+        """
         if track == 'background_music' or track == 'game_over':
             self.son[track].set_volume(0.3)
             self.son[track].play(loops=-1)
@@ -24,10 +42,28 @@ class Soundplayer:
             self.son[track].play()
 
     def stop_sound(self, track):
+        """
+        Fonction: stop_sound
+        --------------------
+        Permet de stopper de la musique/sons
+
+        :param:
+            track: (str) musique présent dans Soundplayer
+        """
         self.son[track].stop()
 
     def pause_sound(self):
+        """
+        Fonction: pause_sound
+        --------------------
+        Permet de pause de la musique/sons
+        """
         pygame.mixer.pause()
 
     def unpause_sound(self):
+        """
+        Fonction: unpause_sound
+        --------------------
+        Permet de reprendre la musique/sons
+        """
         pygame.mixer.unpause()
