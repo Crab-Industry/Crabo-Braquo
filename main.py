bdcb6dc42289574c9ae3f713f870a69151626c80
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2022-05-25 00:34:44 +0200

    Commentary main.py

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
-import time
+"""
+Fichier principal du projet
+Fichier à lancer pour lancer le jeu
+Auteurs : Crabe Industry© 2022 | Groupe F-4
+"""
 import pygame
 
 from jeu import Jeu
 
-# initialisation de pygame
+# Initialisation du module pygame
 pygame.init()
-# class de notre jeu
 
-# creation écran
-pygame.display.set_caption("Crabo-Braquo")  # possibilité de rajouté une icone dans la fenètre en mettant une virgule
+pygame.display.set_caption("Crabo-Braquo")
 screen = pygame.display.set_mode((1000, 394))
 
-# importer un fond d'écran
+# Importation de ressources (fond-d'écran et logo app)
 image_fond = pygame.image.load("assets/picture/desert.jpg")
 icone = pygame.image.load("assets/picture/crabe.png")
 
-# On charge la class jeu
+# Chargement de la classe jeu
 jeu = Jeu()
 
 running = True
 tuto_menu = False
 
 while running:
-    # Icone
+    # Icône
     pygame.display.set_icon(icone)
 
-    # appliquer l'image sur l'arrière plan du jeu
+    # Image d'arrière-plan du jeu
     screen.blit(image_fond, (0, 0))
 
-    # appliquer l'image de lancement du jeu le bouton start aussi
+    # Génération des ressources
     image_start = pygame.image.load("assets/picture/CraboBraquo.png")
     image_start = pygame.transform.scale(image_start, (533, 165))
     logo_rectangle_x = (screen.get_width() / 4)
@@ -36,20 +38,20 @@ while running:
 
     start = pygame.image.load("assets/picture/start2.png")
     start = pygame.transform.scale(start, (265, 81))
-    start_rectangle = start.get_rect()  # on recupère le rectangle
-    start_rectangle.x = (screen.get_width() / 9)    # on utiliser pour savoir si on clique dessus
+    start_rectangle = start.get_rect()
+    start_rectangle.x = (screen.get_width() / 9)
     start_rectangle.y = (screen.get_height() / 1.75)
 
     tuto = pygame.image.load("assets/picture/tuto.png")
     tuto = pygame.transform.scale(tuto, (265, 81))
     tuto_rectangle = tuto.get_rect()
-    tuto_rectangle.x = (screen.get_width() / 2.55)    # on utiliser pour savoir si on clique dessus
+    tuto_rectangle.x = (screen.get_width() / 2.55)
     tuto_rectangle.y = (screen.get_height() / 1.75)
 
     quitter = pygame.image.load("assets/picture/quitter.png")
     quitter = pygame.transform.scale(quitter, (265, 81))
     quitter_rectangle = quitter.get_rect()
-    quitter_rectangle.x = (screen.get_width() / 1.5)    # on utiliser pour savoir si on clique dessus
+    quitter_rectangle.x = (screen.get_width() / 1.5)
     quitter_rectangle.y = (screen.get_height() / 1.75)
 
     tuto_png = pygame.image.load("assets/picture/tuto_png.png")
@@ -59,6 +61,7 @@ while running:
     tuto_png_rectangle.y = 0
 
     if jeu.muraille.vie <= 0:
+        # Fenêtre de game_over stats
         jeu.game_over_stats(screen)
         if jeu.game_over_statement is True:
             jeu.game_over_stats(screen)
@@ -67,32 +70,35 @@ while running:
             jeu.game_over_reset()
     elif jeu.lancement:
         if jeu.pause:
-            # Fenêtre de pause à implémenter
+            # Fenêtre de pause
             jeu.pause_update(screen)
         else:
+            # Fenêtre du jeu
             jeu.update(screen)
     elif tuto_menu:
+        # Fenêtre du tuto
         screen.blit(tuto_png, (tuto_png_rectangle.x, tuto_png_rectangle.y))
     else:
+        # Menu principal
         screen.blit(image_start, (logo_rectangle_x, logo_rectangle_y))
         screen.blit(start, (start_rectangle.x, start_rectangle.y))
         screen.blit(tuto, (tuto_rectangle.x, tuto_rectangle.y))
         screen.blit(quitter, (quitter_rectangle.x,  quitter_rectangle.y))
 
-    # mettre a jour l'écran
+    # Permet la mise à jour de la fenêtre
     pygame.display.flip()
 
-    # condition pour fermer la fenêtre
+    # Détection de fermeture de la fenêtre
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
             quit()
 
-        # detection touche clavier
+        # Détection de l'action du clavier
         elif event.type == pygame.KEYDOWN:
             jeu.pressed[event.key] = True
-            # si la barre espace est appuyer on lance un boulet
+            # Permet l'interaction avec la barre espace
             if event.key == pygame.K_SPACE:
                 if jeu.lancement is True and jeu.pause is False:
                     jeu.canon.lancer_boulet()
@@ -110,7 +116,7 @@ while running:
         elif event.type == pygame.KEYUP:
             jeu.pressed[event.key] = False
 
-        # clique de souris
+        # Permet l'interaction entre la souris et les buttons
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if start_rectangle.collidepoint(event.pos) and jeu.lancement is False and jeu.game_over_statement is False:
                 jeu.start_jeu()
