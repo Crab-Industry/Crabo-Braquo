8c083219fb74d3565afe600fc622ddb2273a0d8e
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2022-05-24 23:08:59 +0200

    Update timer.py
    Remove function unusued add_clock

--- timer.py
+++ timer.py
@@ -1,21 +1,41 @@
+"""
+Fichier de la classe timer.py
+"""
+
 import pygame
 
 
 class Timer:
+    """
+    Classe Timer
+    Permet la gestion du temps, secondes par secondes.
+    """
     def __init__(self):
-        # Timer
+        """
+        Fonction: __init__
+        --------------------
+        self.counter: variable qui compte le nombre de secondes
+        self.mytimerevent:
+        self.clock: clock initialisé depuis le module pygame
+        """
         self.counter = 1
         self.mytimerevent = pygame.USEREVENT + 1
         self.clock = pygame.time.Clock()
         pygame.time.set_timer(self.mytimerevent, 1000)
 
     def update_time(self):
-        # Timer
+        """
+        Fonction: update_time
+        --------------------
+        Fonction qui permet l'ajout de 1 au counter (pour le counter vue joueur)
+        """
         self.counter += 1
         self.clock.tick(60)
 
     def reset_time(self):
+        """
+        Fonction: reset_time
+        --------------------
+        Permet le reset du counter
+        """
         self.counter = 0
-
-    def add_clock(self):
-        self.counter += 1
