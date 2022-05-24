be22a64654398ada23117c0790c7adc6412826ef
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2022-05-25 00:12:01 +0200

    Commentary on boulet.py

--- boulet.py
+++ boulet.py
@@ -1,11 +1,36 @@
+"""
+Fichier de la classe boulet qui contient le boulet
+Image du crabe: "assets/picture/boulet.png"
+Import de math pour les équations de trajectoires
+"""
 import pygame
-
 import math
 
 
-# Class boulet
 class Boulet(pygame.sprite.Sprite):
+    """
+    Classe Boulet
+    Permet le contrôle de la trajectoire du boulet
+    Est généralement appelé par canon.py
+    Possède plusieurs fonctions:
+        __init__()
+        remove()
+        mouvement()
+    """
+
     def __init__(self, canon):
+        """
+        Fonction: __init__
+        --------------------
+        self.canon: Classe Canon() déjà initialisé
+        self.image: Image de la boulet, puis transformée en dimension 40*40
+        self.rect: masque_rectangle du boulet
+        self.rect.x: position x
+        self.rect.y: position y
+        self.y_save: duplication de self.rect.y
+        self.angle: orientation du cannon (importé depuis canon.py, converti de degré en radian)
+        self.velocite: vitesse du boulet
+        """
         self.canon = canon
         super().__init__()
         self.image = pygame.image.load("assets/picture/boulet.png")
@@ -14,18 +39,31 @@ class Boulet(pygame.sprite.Sprite):
         self.rect.x = canon.rect.x + 10
         self.rect.y = canon.rect.y + 10
         self.y_save = self.rect.y
-        #on convertis l'angle de dégrés à radians, faut faire en sorte que ça fonctionne pour les nb négatifs aussi
-        self.angle = (self.canon.angle_canon * math.pi )/ 180 #self.canon.angle_canon
+        # on convertis l'angle de dégrés à radians, faut faire en sorte que ça fonctionne pour les nb négatifs aussi
+        self.angle = (self.canon.angle_canon * math.pi) / 180  # self.canon.angle_canon
         self.velocite = 100
 
     def remove(self):
+        """
+        Fonction: remove()
+        --------------------
+        Suppression du boulet
+        """
         self.canon.all_boulet.remove(self)
 
     def mouvement(self):
+        """
+        Fonction: mouvement()
+        --------------------
+        Calcul de trajectoire et appel à self.remove() si collisions avec un monstre
+        g = Valeur de la pesanteur à la surface de la Terre
+        self.y_save = Hauteur du canon
+        self.angle = Angle par rapport à l’horizontale
+        """
         g = 9.81
         self.rect.x += 10
-        self.rect.y = g / (2 * (self.velocite ** 2) * (math.cos(self.angle)) ** 2) * (self.rect.x ** 2) + (math.tan(self.angle) * self.rect.x) + self.y_save
-
+        self.rect.y = g / (2 * (self.velocite ** 2) * (math.cos(self.angle)) ** 2) * (self.rect.x ** 2) + (
+                    math.tan(self.angle) * self.rect.x) + self.y_save
 
         # supprimer le boulet quand il touche le monstre, infliger des dégats à tous les monstre
         for monstre in self.canon.jeu.collision(self, self.canon.jeu.all_monstres):
@@ -35,5 +73,5 @@ class Boulet(pygame.sprite.Sprite):
             self.remove()
             boss.degat_subit(self.canon.attaque)
         # détruire les boulet sortie de l'écran
-        if self.rect.x > 1000 or self.rect.x < 0 or self.rect.y > 320 or self.rect.y<-50:
+        if self.rect.x > 1000 or self.rect.x < 0 or self.rect.y > 320 or self.rect.y < -50:
             self.remove()
