1dcc932f3198b4c72915d94ed59f3695c3139aa0
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2022-05-24 23:25:12 +0200

    Commentary on muraille.py

--- muraille.py
+++ muraille.py
@@ -1,8 +1,27 @@
+"""
+Fichier de la classe Muraille
+Image de la muraille: "assets/picture/muraille.png"
+"""
 import pygame
 
 
 class Muraille(pygame.sprite.Sprite):
+    """
+    Classe Muraille
+    Muraille du jeu
+    """
     def __init__(self, jeu):
+        """
+        Fonction: __init__
+        --------------------
+        self.vie: HP de la muraille
+        self.max_vie: HP_max de la muraille
+        self.image: Image de la muraille, puis transformée en dimension 200*200
+        self.rect: masque_rectangle de la muraille
+        self.rect.x: position x
+        self.rect.y: position y
+        self.jeu: Import de la classe jeu déjà initialisée
+        """
         super().__init__()
         self.vie = 200
         self.max_vie = 200
@@ -14,10 +33,27 @@ class Muraille(pygame.sprite.Sprite):
         self.jeu = jeu
 
     def barre_de_vie(self, surface):
+        """
+        Fonction: barre_de_vie
+        --------------------
+        Permet d'afficher la barre de vie de la muraille
+        Dessiné par pygame.draw.rect()
+
+        position: barre de self.vie
+        position2: barre de self.max_vie
+        """
         position = [self.rect.x, self.rect.y - 50, self.vie, 8]  # position en x,y la taille et la hauteur
         position2 = [self.rect.x, self.rect.y - 50, self.max_vie, 8]
         pygame.draw.rect(surface, (222, 18, 18), position2)  # barre en arrière plan comme ça on voit la vie max
         pygame.draw.rect(surface, (122, 246, 18), position)  # barre principale
 
     def degat_subit(self, degats):
+        """
+        Fonction: degat_subit
+        --------------------
+        Fonction qui permet la soustraction de la vie en fonction de dégâts
+
+        :param:
+            degats: (int) dégâts infligés à la muraille
+        """
         self.vie -= degats
