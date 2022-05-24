59d15e6228a281f4fcecc930d0bd456d6e0a9f12
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2022-05-24 17:41:32 +0200

    Update monstre.py

--- monstre.py
+++ monstre.py
@@ -39,4 +39,4 @@ class Monstre(pygame.sprite.Sprite):
             # on supprime le crabe quand il a plus de vie
             self.jeu.all_monstres.remove(self)
             # on augmente le score
-            self.jeu.scoreplus(score=5)
+            self.jeu.add_score(score=5)
